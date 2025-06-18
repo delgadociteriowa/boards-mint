c3ae0045e90aad972bd77bafdc3078ffa9e910f1
Carlos <delgadociteriowa@gmail.com>
2025-06-18 17:16:36 -0300

    feat: setSquareColor function

--- src/components/Octoboard.tsx
+++ src/components/Octoboard.tsx
@@ -9,22 +9,49 @@ type BoardGridType = BoardPiece[][];
 
 const Octoboard = () => {
   const [chessGrid, setChessGrid] = useState(() => {
-  const letters = ['a','b','c','d','e','f','g','h'];
-  const grid: BoardGridType = Array(12).fill(null).map((_, rowIndex) =>
-    Array(8).fill(null).map((_, colIndex) => { 
-      return { id: `s${letters[colIndex]}-${rowIndex + 1}` };
-    })
+    const letters = ['a','b','c','d','e','f','g','h'];
+    const grid: BoardGridType = Array(12).fill(null).map((_, rowIndex) =>
+      Array(8).fill(null).map((_, colIndex) => { 
+        return { id: `s${letters[colIndex]}-${rowIndex + 1}` };
+      })
+    );
+    return grid;
+    }
   );
-  return grid;
-  });
-  // add grid ids  bg-teal-950 hover:bg-teal-900  bg-teal-800 hover:bg-teal-700
+
+  const squareBaseStyle = 'aspect-square min-w-6 min-h-6 flex items-center justify-center';
+  
+  const colorDiscard = `bg-teal-800 hover:bg-teal-700`;
+  const colorDiscardDark = `bg-teal-950 hover:bg-teal-900`;
+  const colorSquare = `bg-teal-400 hover:bg-teal-300`;
+  const colorSquareDark = `bg-teal-600 hover:bg-teal-500`;
+
+  const setSquareColor = (row: number, col: number) :string => {
+    let color :string = '';
+    if (row === 0 || row === 11) {
+      color = col % 2 === 0 ? colorDiscardDark : colorDiscard;
+    }
+    if (row === 1 || row === 10) {
+      color = col % 2 === 0 ? colorDiscard : colorDiscardDark;
+    }
+    if (row >= 2 && row <= 9) {
+      if (row % 2 === 0) {
+        color = col % 2 === 0 ? colorSquareDark : colorSquare;
+      } else {
+        color = col % 2 === 0 ? colorSquare : colorSquareDark;
+      }
+    }
+    return color
+  }
+
   return (
     <main className="w-[100%] md:w-[80%] my-0 mx-auto">
       <div className="grid w-[90%] rounded-2xl board-areas overflow-hidden mt-2 mb-8 mx-auto landscape:w-[75%]">
       {chessGrid.map((row, rowIndex) => (
-        row.map((cellContent, colIndex) => (
-          <div key={cellContent?.id} className={`${cellContent?.id} aspect-square min-w-6 min-h-6 flex items-center justify-center outline-1`}></div>
-        ))
+        row.map((cellContent, colIndex) =>{
+          let color = setSquareColor(rowIndex, colIndex);
+          return (<div key={cellContent?.id} className={`${cellContent?.id} ${color} ${squareBaseStyle}`}></div>)
+        })
       ))}
       </div>
     </main>
