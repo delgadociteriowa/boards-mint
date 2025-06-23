dd0461f9d8d72287763547e2248142443c6259c6
Carlos <delgadociteriowa@gmail.com>
2025-06-23 14:56:26 -0300

    fix: hover color fix

--- src/components/OctoBoardSquare.tsx
+++ src/components/OctoBoardSquare.tsx
@@ -8,15 +8,16 @@ interface CellContent {
 interface OctoBoardSquareProps {
   cellContent: CellContent;
   color: string;
+  colorHover: string;
   onClickPiece: (cell: CellContent) => void;
   phaseTwo: boolean;
 }
 
-const OctoBoardSquare: React.FC<OctoBoardSquareProps> = ({cellContent, color, onClickPiece, phaseTwo}) => {
+const OctoBoardSquare: React.FC<OctoBoardSquareProps> = ({cellContent, color, colorHover, onClickPiece, phaseTwo}) => {
   const squareBaseStyle = 'aspect-square min-w-6 min-h-6 flex items-center justify-center';
   const validClickable = (cellContent.piece && !phaseTwo) || phaseTwo;
   return (
-    <div className={`${cellContent.id} ${color} ${squareBaseStyle} ${validClickable && 'cursor-pointer'}`} onClick={() => validClickable && onClickPiece(cellContent)}>
+    <div className={`${cellContent.id} ${color} ${cellContent.piece && colorHover} ${squareBaseStyle} ${validClickable && 'cursor-pointer'}`} onClick={() => validClickable && onClickPiece(cellContent)}>
       {
         cellContent.piece.includes('checker') ?
           <div className={`${cellContent.pieceType.includes('one') ? 'bg-rose-400' : 'bg-stone-600'} relative w-[60%] h-[60%] rounded-full checker-shadow`}></div>
--- src/components/Octoboard.tsx
+++ src/components/Octoboard.tsx
@@ -13,13 +13,16 @@ type BoardPiece = {
 }; 
 type BoardGridType = BoardPiece[][];
 
+type ColorsType = {
+  chess: string[];
+  checkers: string[];
+}
+
 interface OctoBoardProps {
   selectedGame: GameNames;
 };
 
 const Octoboard: React.FC<OctoBoardProps> = ({selectedGame}) => {
-
-  
 
   const buildGameGrid = () => {
     if (selectedGame === 'checkers') {
@@ -96,96 +99,46 @@ const Octoboard: React.FC<OctoBoardProps> = ({selectedGame}) => {
 
   
   const gameColors = {
-    chess: ['bg-teal-950 hover:bg-teal-900','bg-teal-800 hover:bg-teal-700','bg-teal-600 hover:bg-teal-500','bg-teal-400 hover:bg-teal-300'],
-    checkers: ['bg-cyan-950 hover:bg-cyan-900','bg-cyan-800 hover:bg-cyan-700','bg-cyan-600 hover:bg-cyan-500','bg-cyan-400 hover:bg-cyan-300']
+    chess: ['bg-teal-950','bg-teal-800','bg-teal-600','bg-teal-400'],
+    checkers: ['bg-cyan-950','bg-cyan-800','bg-cyan-600','bg-cyan-400']
+  };
+  
+  const gameColorsHover = {
+    chess: ['hover:bg-teal-900','hover:bg-teal-700','hover:bg-teal-500','hover:bg-teal-300'],
+    checkers: ['hover:bg-cyan-900','hover:bg-cyan-700','hover:bg-cyan-500','hover:bg-cyan-300']
   };
 
-  const setSquareColor = (row: number, col: number) :string => {
+  const setSquareColor = (row: number, col: number, colors: ColorsType) :string => {
     let color :string = '';
     if (row === 0 || row === 11) {
-      color = col % 2 === 0 ? gameColors[selectedGame][1] : gameColors[selectedGame][0];
+      color = col % 2 === 0 ? colors[selectedGame][1] : colors[selectedGame][0];
     }
     if (row === 1 || row === 10) {
-      color = col % 2 === 0 ? gameColors[selectedGame][0]: gameColors[selectedGame][1];
+      color = col % 2 === 0 ? colors[selectedGame][0]: colors[selectedGame][1];
     }
     if (row >= 2 && row <= 9) {
       if (row % 2 === 0) {
-        color = col % 2 === 0 ? gameColors[selectedGame][3] : gameColors[selectedGame][2];
+        color = col % 2 === 0 ? colors[selectedGame][3] : colors[selectedGame][2];
       } else {
-        color = col % 2 === 0 ? gameColors[selectedGame][2] : gameColors[selectedGame][3];
+        color = col % 2 === 0 ? colors[selectedGame][2] : colors[selectedGame][3];
       }
     }
     return color
   }
-
-  const setNewChess = () => {
-    setGameGrid(prevGrid => {
-        const newGrid = prevGrid.map(row => [...row]);
-        const chessPieces = ['♜','♞','♝','♛','♚','♝','♞','♜'];
-        const setCells = (cellsInRow: BoardPiece[], rowIndex: number) => {
-          return cellsInRow.map((cell, index) => {
-            const isPawnRow = (rowIndex === 3 || rowIndex === 8);
-            const isPlayerOneRow = (rowIndex === 2 || rowIndex === 3); 
-            return {
-              ...cell,
-              piece: isPawnRow ? '♟' : chessPieces[index],
-              pieceType: isPlayerOneRow ? 'one' : 'two',
-            };
-          });
-        };
-        newGrid[2] = setCells(newGrid[2], 2);
-        newGrid[3] = setCells(newGrid[3], 3);
-        newGrid[8] = setCells(newGrid[8], 8);
-        newGrid[9] = setCells(newGrid[9], 9);
-        return newGrid;
-      }
-    );
-  };
-  
-  const setNewCheckers = () => {
-    setGameGrid(prevGrid => {
-        const newGrid = prevGrid.map(row => [...row]);
-        const setCells = (cellsInRow: BoardPiece[], rowIndex: number) => {
-          return cellsInRow.map((cell, index) => {
-            const isPlayerOneRow = (rowIndex === 2 || rowIndex === 3 || rowIndex === 4);
-            const isRowIndexEven = (rowIndex === 2 || rowIndex === 4 || rowIndex === 8);
-            const isEven = (index + 1) % 2 === 0;
-            return {
-              ...cell,
-              piece: isRowIndexEven ? (isEven ? 'checker' : '') : (isEven ? '' : 'checker'),
-              pieceType: isRowIndexEven ? (isEven ? (isPlayerOneRow ? 'one' : 'two') : '') : (isEven ? '' : (isPlayerOneRow ? 'one' : 'two')),
-            };
-            
-          });
-        };
-        newGrid[2] = setCells(newGrid[2], 2);
-        newGrid[3] = setCells(newGrid[3], 3);
-        newGrid[4] = setCells(newGrid[4], 4);
-        newGrid[7] = setCells(newGrid[7], 7);
-        newGrid[8] = setCells(newGrid[8], 8);
-        newGrid[9] = setCells(newGrid[9], 9);
-        return newGrid;
-      }
-    );
-  };
 
   const onClickPiece = (cell: BoardPiece) => {
     console.log(cell.id);
     console.log(cell.piece);
   }
 
-  // useEffect(() => {
-  //   if (selectedGame === 'chess') setNewChess();
-  //   if (selectedGame === 'checkers') setNewCheckers();
-  // }, []);
-
   return (
     <main className="w-[100%] md:w-[80%] my-0 mx-auto">
       <div className="grid w-[90%] rounded-2xl board-areas overflow-hidden mt-2 mb-8 mx-auto landscape:w-[75%]">
       {gameGrid.map((row, rowIndex) => (
         row.map((cellContent, colIndex) =>{
-          let color = setSquareColor(rowIndex, colIndex);
-          return (<OctoBoardSquare key={cellContent.id} cellContent={cellContent} color={color} onClickPiece={onClickPiece} phaseTwo={phaseTwo}/>)
+          let color = setSquareColor(rowIndex, colIndex, gameColors);
+          let colorHover = setSquareColor(rowIndex, colIndex, gameColorsHover);
+          return (<OctoBoardSquare key={cellContent.id} cellContent={cellContent} color={color} colorHover={colorHover} onClickPiece={onClickPiece} phaseTwo={phaseTwo}/>)
         })
       ))}
       </div>
