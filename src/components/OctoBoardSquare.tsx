730e2f6e23424f5ee5f028f199502bebd07f2056
Carlos <delgadociteriowa@gmail.com>
2025-06-23 14:20:25 -0300

    feat: implement buildGameGrid and fix color issues

--- src/components/OctoBoardSquare.tsx
+++ src/components/OctoBoardSquare.tsx
@@ -19,9 +19,9 @@ const OctoBoardSquare: React.FC<OctoBoardSquareProps> = ({cellContent, color, on
     <div className={`${cellContent.id} ${color} ${squareBaseStyle} ${validClickable && 'cursor-pointer'}`} onClick={() => validClickable && onClickPiece(cellContent)}>
       {
         cellContent.piece.includes('checker') ?
-          <div className={`${cellContent.pieceType.includes('one') ? 'bg-stone-600' : 'bg-rose-400'} relative w-[60%] h-[60%] rounded-full checker-shadow`}></div>
+          <div className={`${cellContent.pieceType.includes('one') ? 'bg-rose-400' : 'bg-stone-600'} relative w-[60%] h-[60%] rounded-full checker-shadow`}></div>
           :
-          <span className={`${cellContent.pieceType === 'one' ? 'text-stone-100' : 'text-stone-800'} piece__size`}>{cellContent.piece}</span>
+          <span className={`${cellContent.pieceType === 'one' ? 'text-stone-800' : 'text-stone-100'} piece__size`}>{cellContent.piece}</span>
       }
     </div>
   )
--- src/components/Octoboard.tsx
+++ src/components/Octoboard.tsx
@@ -18,27 +18,86 @@ interface OctoBoardProps {
 };
 
 const Octoboard: React.FC<OctoBoardProps> = ({selectedGame}) => {
+
+  
+
+  const buildGameGrid = () => {
+    if (selectedGame === 'checkers') {
+      const checkerPiece = (row: number, col: number): [string, string] => {
+        const isPlayerOne = [2, 3, 4].includes(row);
+        const isPlayerTwo = [7, 8, 9].includes(row);
+        const isEvenCol = (col + 1) % 2 === 0;
+
+        const shouldPlace =
+          (isPlayerOne && ((row === 3 && !isEvenCol) || (row !== 3 && isEvenCol))) ||
+          (isPlayerTwo && ((row === 8 && isEvenCol) || (row !== 8 && !isEvenCol)));
+
+        const piece = shouldPlace ? 'checker' : '';
+        const pieceType = shouldPlace ? (isPlayerOne ? 'one' : 'two') : '';
+                
+        return [piece, pieceType]
+      }
+
+      return Array(12).fill(null).map((_, rowIndex) =>
+        Array(8).fill(null).map((_, colIndex) => { 
+          return { 
+            id: `sqr${colIndex}-${rowIndex}`,
+            piece: checkerPiece(rowIndex, colIndex)[0],
+            pieceType: checkerPiece(rowIndex, colIndex)[1],
+            selected: false
+          };
+        })
+      );
+    }
+
+    const chessPiece = (row: number, col: number): [string, string] => {
+      const chessPieces = ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'];
+      let piece = '';
+      let pieceType = '';
+
+      switch (row) {
+        case 2:
+          piece = chessPieces[col] || '';
+          pieceType = 'one';
+          break;
+        case 3:
+          piece = '♟';
+          pieceType = 'one';
+          break;
+        case 8:
+          piece = '♟';
+          pieceType = 'two';
+          break;
+        case 9:
+          piece = chessPieces[col] || '';
+          pieceType = 'two';
+          break;
+        default:
+          break;
+      }
+      return [piece, pieceType];
+    };
+
+    return Array(12).fill(null).map((_, rowIndex) => 
+      Array(8).fill(null).map((_, colIndex) => {
+          return { 
+            id: `sqr${colIndex}-${rowIndex}`,
+            piece: chessPiece(rowIndex, colIndex)[0],
+            pieceType: chessPiece(rowIndex, colIndex)[1],
+            selected: false
+          };
+        })
+    );
+  };
+
+  const [gameGrid, setGameGrid] = useState(() => buildGameGrid());
   const [phaseTwo, setPhaseTwo] = useState(false);
   const [selectedPiece, setSelectedPiece] = useState(false);
 
-  const [gameGrid, setGameGrid] = useState(() => {
-    const grid: BoardGridType = Array(12).fill(null).map((_, rowIndex) =>
-      Array(8).fill(null).map((_, colIndex) => { 
-        return { 
-          id: `sqr${colIndex}-${rowIndex}`,
-          piece: '',
-          pieceType: '',
-          selected: false
-        };
-      })
-    );
-    return grid;
-    }
-  );
-
+  
   const gameColors = {
-    chess: ['bg-teal-800 hover:bg-teal-700', 'bg-teal-950 hover:bg-teal-900', 'bg-teal-400 hover:bg-teal-300', 'bg-teal-600 hover:bg-teal-500'],
-    checkers: ['bg-cyan-800 hover:bg-cyan-700', 'bg-cyan-950 hover:bg-cyan-900', 'bg-cyan-400 hover:bg-cyan-300', 'bg-cyan-600 hover:bg-cyan-500']
+    chess: ['bg-teal-950 hover:bg-teal-900','bg-teal-800 hover:bg-teal-700','bg-teal-600 hover:bg-teal-500','bg-teal-400 hover:bg-teal-300'],
+    checkers: ['bg-cyan-950 hover:bg-cyan-900','bg-cyan-800 hover:bg-cyan-700','bg-cyan-600 hover:bg-cyan-500','bg-cyan-400 hover:bg-cyan-300']
   };
 
   const setSquareColor = (row: number, col: number) :string => {
@@ -115,10 +174,10 @@ const Octoboard: React.FC<OctoBoardProps> = ({selectedGame}) => {
     console.log(cell.piece);
   }
 
-  useEffect(() => {
-    if (selectedGame === 'chess') setNewChess();
-    if (selectedGame === 'checkers') setNewCheckers();
-  }, []);
+  // useEffect(() => {
+  //   if (selectedGame === 'chess') setNewChess();
+  //   if (selectedGame === 'checkers') setNewCheckers();
+  // }, []);
 
   return (
     <main className="w-[100%] md:w-[80%] my-0 mx-auto">
