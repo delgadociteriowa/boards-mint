03505888d353a88b7b3325b0a35005916c0572b4
Carlos <delgadociteriowa@gmail.com>
2025-06-23 16:14:02 -0300

    fix: remove function

--- src/components/OctoBoardSquare.tsx
+++ src/components/OctoBoardSquare.tsx
@@ -13,10 +13,10 @@ interface OctoBoardSquareProps {
 
 // onClickPiece: (cell: CellContent) => void;
 
-const OctoBoardSquare: React.FC<OctoBoardSquareProps> = ({cellContent, color, colorHover, onClickPiece}) => {
+const OctoBoardSquare: React.FC<OctoBoardSquareProps> = ({cellContent, color, colorHover}) => {
   const squareBaseStyle = 'aspect-square min-w-6 min-h-6 flex items-center justify-center';
   return (
-    <div className={`${cellContent.id} ${color} ${cellContent.piece && colorHover} ${squareBaseStyle} ${cellContent.piece && 'cursor-pointer'}`} onClick={() => validClickable && onClickPiece(cellContent)}>
+    <div className={`${cellContent.id} ${color} ${cellContent.piece && colorHover} ${squareBaseStyle} ${cellContent.piece && 'cursor-pointer'}`}>
       {
         cellContent.piece.includes('checker') ?
           <div className={`${cellContent.pieceType.includes('one') ? 'bg-rose-400' : 'bg-stone-600'} relative w-[60%] h-[60%] rounded-full checker-shadow`}></div>
