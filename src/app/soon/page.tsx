8f2f9a98d53c7b06da45f1f4a8af0d1365c8d025
Carlos <delgadociteriowa@gmail.com>
2025-06-24 18:32:13 -0300

    fix: small link fixes

--- src/app/soon/page.tsx
+++ src/app/soon/page.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Construction from "../../assets/construction.svg";
+import Link from "next/link";
 
 const Soon = () => {
   return (
@@ -14,7 +15,7 @@ const Soon = () => {
             <figure className="h-40 mx-0 my-16">
               <Image src={Construction} alt="Under construction image" className="block h-[100%] mx-auto my-0" width={500} height={500}/>
             </figure>
-            <a href="/" className="block w-[15rem] bg-stone-300 hover:bg-stone-200 py-6 rounded-full text-center no-underline text-stone-800 text-xl lowercase tracking-[3px] mx-auto my-0 shadow-lg">Home</a>
+            <Link href="/" className="block w-[15rem] bg-stone-300 hover:bg-stone-200 py-6 rounded-full text-center no-underline text-stone-800 text-xl lowercase tracking-[3px] mx-auto my-0 shadow-lg">Home</Link>
           </section>
         </main>
       <Footer/>
