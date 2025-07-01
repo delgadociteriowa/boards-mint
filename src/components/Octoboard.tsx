19e7d85bf46385415f5923fa529813d642af5bd6
Carlos <delgadociteriowa@gmail.com>
2025-07-01 12:10:32 -0300

    fix: buildGameGrid function improvement

--- src/components/Octoboard.tsx
+++ src/components/Octoboard.tsx
@@ -24,63 +24,63 @@ interface OctoBoardProps {
 
 const Octoboard: React.FC<OctoBoardProps> = ({selectedGame}) => {
 
-  const buildGameGrid = () => {
+  const placeChessPiece = (row: number, col: number): [string, string] => {
+    const playerOnePawnsRow = 3;
+    const playerTwoPawnsRow = 8;
+    const playerOneChessRow = 2;
+    const playerTwoChessRow = 9;
+
+    const chessPieces = ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'];
+    let piece = '';
+    let pieceType = '';
+
+    switch (row) {
+      case playerOneChessRow:
+        piece = chessPieces[col] || '';
+        pieceType = 'one';
+        break;
+      case playerOnePawnsRow:
+        piece = '♟';
+        pieceType = 'one';
+        break;
+      case playerTwoPawnsRow:
+        piece = '♟';
+        pieceType = 'two';
+        break;
+      case playerTwoChessRow:
+        piece = chessPieces[col] || '';
+        pieceType = 'two';
+        break;
+      default:
+        break;
+    }
+    return [piece, pieceType];
+  };
+
+  const placeCheckerPiece = (row: number, col: number): [string, string] => {
+    const playerOneRows = [2, 3, 4];
+    const playerTwoRows = [7, 8, 9];
+    const isPlayerOne = playerOneRows.includes(row);
+    const isPlayerTwo = playerTwoRows.includes(row);
+    const isEvenCol = (col + 1) % 2 === 0;
+
+    const shouldPlace =
+      (isPlayerOne && ((row === 3 && !isEvenCol) || (row !== 3 && isEvenCol))) ||
+      (isPlayerTwo && ((row === 8 && isEvenCol) || (row !== 8 && !isEvenCol)));
+
+    const piece = shouldPlace ? 'checker' : '';
+    const pieceType = shouldPlace ? (isPlayerOne ? 'one' : 'two') : '';
+            
+    return [piece, pieceType]
+  }
+
+  const buildGameGrid = () :Square[][] => {
     const boardRows = 12;
     const boardColumns = 8;
 
-    const chessPiece = (row: number, col: number): [string, string] => {
-      const playerOnePawnsRow = 3;
-      const playerTwoPawnsRow = 8;
-      const playerOneChessRow = 2;
-      const playerTwoChessRow = 9;
-
-      const chessPieces = ['♜', '♞', '♝', '♛', '♚', '♝', '♞', '♜'];
-      let piece = '';
-      let pieceType = '';
-
-      switch (row) {
-        case playerOneChessRow:
-          piece = chessPieces[col] || '';
-          pieceType = 'one';
-          break;
-        case playerOnePawnsRow:
-          piece = '♟';
-          pieceType = 'one';
-          break;
-        case playerTwoPawnsRow:
-          piece = '♟';
-          pieceType = 'two';
-          break;
-        case playerTwoChessRow:
-          piece = chessPieces[col] || '';
-          pieceType = 'two';
-          break;
-        default:
-          break;
-      }
-      return [piece, pieceType];
-    };
-    
-    const checkerPiece = (row: number, col: number): [string, string] => {
-      const playerOneRows = [2, 3, 4];
-      const playerTwoRows = [7, 8, 9];
-      const isPlayerOne = playerOneRows.includes(row);
-      const isPlayerTwo = playerTwoRows.includes(row);
-      const isEvenCol = (col + 1) % 2 === 0;
-
-      const shouldPlace =
-        (isPlayerOne && ((row === 3 && !isEvenCol) || (row !== 3 && isEvenCol))) ||
-        (isPlayerTwo && ((row === 8 && isEvenCol) || (row !== 8 && !isEvenCol)));
-
-      const piece = shouldPlace ? 'checker' : '';
-      const pieceType = shouldPlace ? (isPlayerOne ? 'one' : 'two') : '';
-              
-      return [piece, pieceType]
-    }
-
     return Array(boardRows).fill(null).map((_, rowIndex) =>
       Array(boardColumns).fill(null).map((_, colIndex) => {
-        const piece = selectedGame === 'chess' ? chessPiece(rowIndex, colIndex) : checkerPiece(rowIndex, colIndex);  
+        const piece = selectedGame === 'chess' ? placeChessPiece(rowIndex, colIndex) : placeCheckerPiece(rowIndex, colIndex);  
         return { 
           id: `sqr${colIndex}-${rowIndex}`,
           piece: piece[0],
