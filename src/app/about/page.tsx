23425fe05a02146a18908ad8ee4252c15a538582
Carlos <delgadociteriowa@gmail.com>
2025-07-13 23:22:16 -0300

    fix: small text fix

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -12,7 +12,7 @@ const Soon: React.FC = () => {
               <p className="text-left text-xl">Welcome to Boards, an easy and free platform with your favorite board games. No hassle, no sign-ups, just pick a game and let the fun begin.</p>
             </div>
             <div className="bg-stone-50 px-14 py-16 rounded-3xl grow shadow-xl/30 my-7">
-              <p className="text-left text-xl">Boards is a simple and accessible virtual replacement for classic board games like chess or checkers, available from any device. Whether you're on a PC, laptop, tablet, or phone, you can start a new game in seconds.</p>
+              <p className="text-left text-xl">Boards is a simple and accessible virtual replacement for classic board games like chess or checkers, available from any device. Whether you&apos;re on a PC, laptop, tablet, or phone, you can start a new game in seconds.</p>
             </div>
             <div className="bg-stone-50 px-14 py-16 rounded-3xl grow shadow-xl/30 my-7">
               <p className="text-left text-xl">Boards has no limitations, so if you want to move a piece somewhere that goes against the rules, go ahead, just like in real life.    Boards gives you the tools to start a game, and the rest is up to you.</p>
