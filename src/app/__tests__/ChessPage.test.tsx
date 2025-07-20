1e66ec846ec06bfaf94c465ebb98e970b93fca40
Carlos <delgadociteriowa@gmail.com>
2025-07-19 21:43:43 -0300

    fix: test fixes

--- src/app/__tests__/ChessPage.test.tsx
+++ src/app/__tests__/ChessPage.test.tsx
@@ -3,24 +3,20 @@ import Chess from '../chess/page';
 import BoardState from '@/context/board/BoardState';
 
 describe('Chess Page COmponent', () => {
-  it('Renders a chess game', () => {
+  beforeEach(() => {
     render(
       <BoardState>
         <Chess />
-      </BoardState>  
+      </BoardState>
     );
+  });
 
+  it('Renders a chess game', () => {
     const towers = screen.getAllByText('♜');
     expect(towers).toHaveLength(4);
   });
   
   it('Moves a piece to an empty space', async () => {
-    render(
-      <BoardState>
-        <Chess />
-      </BoardState>  
-    );
-
     const selectedPiece = screen.getByTestId('sqr0-2');
     const selectedEmptySqr = screen.getByTestId('sqr0-4');
     fireEvent.click(selectedPiece);
