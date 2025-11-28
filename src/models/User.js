86a39771bf2701bbbc2a516c754f2de550e997b3
Carlos <delgadociteriowa@gmail.com>
2025-11-28 17:24:06 -0300

    next auth instalado, ruta auth creada, authOptions en utils base creado y pasado a la ruta, authprovider wrapper creado en components, layout wrap con authprovider

--- src/models/User.js
+++ src/models/User.js
@@ -1,10 +1,12 @@
 import { Schema, model, models } from 'mongoose';
+import { unique } from 'next/dist/build/utils';
 
 const UserSchema = new Schema(
   {
     username: {
       type: String,
       required: [true, 'Username is required'],
+      unique: [true, 'Username is not unique'],
     },
     password: {
       type: String,
