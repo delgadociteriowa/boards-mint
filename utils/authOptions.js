18e89c07e24c5a5de20b889abaace0f621b60d76
Carlos <delgadociteriowa@gmail.com>
2025-11-28 17:44:53 -0300

    feat: variables nextauth en .env, authOptions con Credentials base

--- utils/authOptions.js
+++ utils/authOptions.js
@@ -1,1 +1,13 @@
-export const authOptions = {};
+import Credentials from 'next-auth/providers/credentials';
+
+export const authOptions = {
+  providers: [
+    Credentials({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+    }),
+  ],
+};
