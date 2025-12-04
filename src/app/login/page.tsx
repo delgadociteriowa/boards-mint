d5545b87ce0378e4138b270cdc6277cbf2980f21
Carlos <delgadociteriowa@gmail.com>
2025-12-04 19:00:50 -0300

    form implemented

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -18,8 +18,10 @@ const Login: React.FC = () => {
     }
   }, [session, router]);
 
-  const handleLogin = async () => {
-     const res = await signIn("credentials", {
+  const handleLogin = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const res = await signIn("credentials", {
       username: userName,
       password: password,
       redirect: false,
@@ -27,7 +29,7 @@ const Login: React.FC = () => {
 
     if (res?.ok) router.push("/account");
   };
-  
+
   return (
     <>
       <Header />
@@ -36,13 +38,14 @@ const Login: React.FC = () => {
           Login
         </h3>
         <section className="w-[90%] mx-auto max-w-[500px] py-20 text-stone-700 mb-40">
-          <div className="flex flex-col gap-8">
+          <form onSubmit={handleLogin} className="flex flex-col gap-8">
             <div className="flex flex-col gap-2">
               <label className="text-stone-600 tracking-[1px] text-sm">
                 username
               </label>
               <input
                 type="text"
+                required
                 value={userName}
                 onChange={(e) => setUserName(e.target.value)}
                 className="border border-stone-300 rounded-xl py-4 px-4 text-stone-700 focus:outline-none focus:ring-2 focus:ring-sky-500"
@@ -54,18 +57,19 @@ const Login: React.FC = () => {
               </label>
               <input
                 type="password"
+                required
                 value={password}
                 onChange={(e) => setPassword(e.target.value)}
                 className="border border-stone-300 rounded-xl py-4 px-4 text-stone-700 focus:outline-none focus:ring-2 focus:ring-sky-500"
               />
             </div>
             <button
-              onClick={handleLogin}
+              type="submit"
               className="bg-sky-600 hover:bg-sky-500 py-5 rounded-full text-center text-stone-100 text-lg tracking-[2px] mt-4 shadow-xl/20"
             >
               login
             </button>
-          </div>
+          </form>
         </section>
       </main>
       <Footer />
