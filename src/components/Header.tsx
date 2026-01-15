8d66dc3307896c845f376b3c26a0df7befaf08f2
Carlos <delgadociteriowa@gmail.com>
2026-01-15 13:56:24 -0300

    feat: header board cleanup

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,27 @@
+'use client';
+import { useEffect } from "react";
+import { usePathname } from "next/navigation";
+import { useAppDispatch } from "@/state/hooks";
+import { closeGame } from "@/state/board/boardSlice";
 import Navigation from "./Navigation";
 
-const Header = () => (
-  <header>
-    <Navigation/>
-  </header>
-);
+const Header = () => {
+  const pathname = usePathname();
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    const gameRoutes = ['/chess', '/checkers'];
+
+    if (!gameRoutes.includes(pathname)) {
+      dispatch(closeGame());
+    }
+  }, [pathname, dispatch]);
+
+  return (
+    <header>
+      <Navigation/>
+    </header>
+  )
+};
 
 export default Header;
