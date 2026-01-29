e0c8af474adea75de5c74390d4b126978371bde0
Carlos <delgadociteriowa@gmail.com>
2026-01-29 19:31:57 -0300

    fix: saved games spiner and fixes

---
+++ src/components/Spinner.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+const Spinner = () => {
+  return (
+    <div className="flex h-full w-full items-center justify-center pt-20">
+      <div className="spinner"/>
+    </div>
+  );
+};
+
+export default Spinner;
