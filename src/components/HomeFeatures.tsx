1d652723d333fb4c396106c5f7a13b80ad7ff7d3
Carlos <delgadociteriowa@gmail.com>
2026-02-18 13:45:59 -0300

    HomeFeatures opt

--- src/components/HomeFeatures.tsx
+++ src/components/HomeFeatures.tsx
@@ -3,27 +3,34 @@ import MobileIcon from "../assets/mobile-screen-solid.svg";
 import HandHeartIcon from "../assets/hand-holding-heart-solid.svg"
 import FeatureCard from "./FeatureCard";
 
-const HomeFeatures = () => (
-    <section className="w-[90%] mt-8 mx-auto max-w-[1200px] pt-8 pb-14 text-stone-600 appear__main__element">
+interface Feature {
+  icon: string;
+  altText: string;
+  title: string;
+  paragraph: string;
+};
+
+const HomeFeatures = () => {
+  const features: Feature[] = [
+    { icon: ChessIcon, altText: 'Chess icon', title: 'No Restrictions', paragraph: 'Place the pieces wherever you want'},
+    { icon: HandHeartIcon, altText: 'A hand holding a heart icon', title: 'Totally Free', paragraph: 'No subscriptions, no payments, no ads'},
+    { icon: MobileIcon, altText: 'Mobile icon', title: 'Play Anywhere', paragraph: 'Play on mobile, tablet or desktop'},
+  ];
+
+  return(<section className="w-[90%] mt-8 mx-auto max-w-[1200px] pt-8 pb-14 text-stone-600 appear__main__element">
       <h3 className="text-center text-4xl tracking-[2px] mb-8">Boards Makes it Easy</h3>
       <div className="py-5 flex flex-wrap justify-center gap-16">
-        <FeatureCard
-          icon={ChessIcon}
-          altText={'Chess icon'}
-          title={'No Restrictions'}
-          paragraph={'Place the pieces wherever you want'}/>
-        <FeatureCard
-          icon={HandHeartIcon}
-          altText={'A hand holding a heart icon'}
-          title={'Totally Free'}
-          paragraph={'No subscriptions, no payments, no ads'}/>
-        <FeatureCard
-          icon={MobileIcon}
-          altText={'Mobile icon'}
-          title={'Play Anywhere'}
-          paragraph={'Play on mobile, tablet or desktop'}/>
+        {features.map(feature => (
+          <FeatureCard
+            key={feature.altText}
+            icon={feature.icon}
+            altText={feature.altText}
+            title={feature.title}
+            paragraph={feature.paragraph}/>
+          ))
+        }
       </div>
-    </section>
-);
+    </section>)
+};
 
 export default HomeFeatures;
