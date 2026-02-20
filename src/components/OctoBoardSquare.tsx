df7f6014dfe83d710daa4e01ae7f8fe571cfc41d
Carlos <delgadociteriowa@gmail.com>
2026-02-20 16:26:11 -0300

    octoBoardSquare cleaned

--- src/components/OctoBoardSquare.tsx
+++ src/components/OctoBoardSquare.tsx
@@ -3,22 +3,15 @@ import { Square } from "@/types/board";
 interface OctoBoardSquareProps {
   cell: Square;
   squareStyle: string;
+  pieceStyle: string;
   onClickPiece: (cell: Square) => void;
 }
 
-const OctoBoardSquare = ({cell, squareStyle, onClickPiece} : OctoBoardSquareProps) => {
+const OctoBoardSquare = ({cell, squareStyle, pieceStyle, onClickPiece} : OctoBoardSquareProps) => {
   const squareBaseStyle = 'aspect-square min-w-6 min-h-6 flex items-center justify-center';
   return (
-    <div
-      data-testid={cell.id}
-      className={`${cell.id} ${squareBaseStyle} ${squareStyle}`}
-      onClick={() => onClickPiece(cell)}>
-      {
-        cell.piece.includes('checker') ?
-          <div className={`${cell.pieceType.includes('one') ? 'bg-rose-400' : 'bg-stone-600'} relative w-[60%] h-[60%] rounded-full checker-shadow`}></div>
-          :
-          <span className={`${cell.pieceType === 'one' ? 'text-stone-800' : 'text-stone-50'} piece__size select-none [text-shadow:2px_2px_4px_rgba(0,0,0,0.3)]`}>{cell.piece}</span>
-      }
+    <div data-testid={cell.id} className={`${cell.id} ${squareBaseStyle} ${squareStyle}`} onClick={() => onClickPiece(cell)}>
+      <div className={`${pieceStyle}`}>{cell.piece.length === 1 && cell.piece}</div>
     </div>
   )
 };
--- src/components/Octoboard.tsx
+++ src/components/Octoboard.tsx
@@ -108,6 +108,13 @@ const Octoboard = () => {
     return squareStyle
   };
 
+  const createPieceStyle = (cell: Square): string => {
+    if(cell.piece.includes('checker')) {
+      return `${cell.pieceType.includes('one') ? 'bg-rose-400' : 'bg-stone-600'} relative w-[60%] h-[60%] rounded-full checker-shadow`
+    }
+    return `${cell.pieceType === 'one' ? 'text-stone-800' : 'text-stone-50'} piece__size select-none [text-shadow:2px_2px_4px_rgba(0,0,0,0.3)]`
+  }
+
   return (
     <>
       {(!gameGrid.length || loading) && !error && <LoadingComponent />}
@@ -119,7 +126,14 @@ const Octoboard = () => {
           {gameGrid.map((row) => (
             row.map((cell) =>{
               const squareStyle = createSquareStyle(cell);  
-              return (<OctoBoardSquare key={cell.id} cell={cell} squareStyle={squareStyle} onClickPiece={handleClickSqr} />)
+              const pieceStyle = createPieceStyle(cell);  
+              return (<OctoBoardSquare
+                key={cell.id}
+                cell={cell}
+                squareStyle={squareStyle}
+                pieceStyle={pieceStyle}
+                onClickPiece={handleClickSqr}
+                />)
             })
           ))}
           </div>
