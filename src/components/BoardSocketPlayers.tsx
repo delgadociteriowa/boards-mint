d465d6e9c87a2859d67c74dfbc0ceac4b5f1e0b7
Carlos <delgadociteriowa@gmail.com>
2026-04-23 22:39:32 -0300

    fix: account and naming fix

--- src/components/BoardSocketPlayers.tsx
+++ src/components/BoardSocketPlayers.tsx
@@ -1,27 +1,40 @@
 'use client';
-import { useAppSelector } from "@/state/hooks";
+import { useAppSelector } from '@/state/hooks';
 
 const BoardSocketPlayers = () => {
-  const {
-    socketActive,
-    socketHost,
-    socketGuest
-  }  = useAppSelector(state => state.board);
-  
+  const { socketActive, socketHost, socketGuest } = useAppSelector(
+    (state) => state.board,
+  );
+
+  const socketHostTrimmed =
+    socketHost.length > 7 ? `${socketHost.slice(0, 7)}...` : socketHost;
+  const socketGuestTrimmed =
+    socketGuest.length > 7 ? `${socketGuest.slice(0, 7)}...` : socketGuest;
+
   return (
     <div className='flex items-center w-[90%] landscape:w-[75%] mx-auto h-6 px-4'>
-      {socketActive &&
-        (<>
-          <div className="flex-1 flex justify-start">
-            <h4 className="font-bold text-sky-500 tracking-[1px]">HOST <span className="font-normal ml-2 text-stone-600 tracking-[0px]">{socketHost}</span></h4>
+      {socketActive && (
+        <>
+          <div className='flex-1 flex justify-start'>
+            <h4 className='font-bold text-sky-500 tracking-[1px]'>
+              HOST{' '}
+              <span className='font-normal ml-2 text-stone-600 tracking-[0px]'>
+                {socketHostTrimmed}
+              </span>
+            </h4>
           </div>
-          <div className="flex-1 flex justify-end">
-            <h4 className="font-bold text-amber-500 tracking-[1px]">GUEST <span className="font-normal ml-2 text-stone-600 tracking-[0px]">{socketGuest}</span></h4>
+          <div className='flex-1 flex justify-end'>
+            <h4 className='font-bold text-amber-500 tracking-[1px]'>
+              GUEST{' '}
+              <span className='font-normal ml-2 text-stone-600 tracking-[0px]'>
+                {socketGuestTrimmed}
+              </span>
+            </h4>
           </div>
-        </>)
-      }
+        </>
+      )}
     </div>
-  )
+  );
 };
 
 export default BoardSocketPlayers;
--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,34 +1,80 @@
 'use client';
-import { useSession } from "next-auth/react";
 import { useAppSelector } from '@/state/hooks';
-import Link from 'next/link';
+import { useSession } from 'next-auth/react';
 import Image from 'next/image';
-import NavigationItem from "./NavigationItem";
-import HamburguerIcon from '../assets/icon-hamburger.svg'
-import CloseIcon from '../assets/icon-close.svg'
+import Link from 'next/link';
+import CloseIcon from '../assets/icon-close.svg';
+import HamburguerIcon from '../assets/icon-hamburger.svg';
+import NavigationItem from './NavigationItem';
 
 const Navigation = () => {
-  const selectedGame = useAppSelector(state => state.board.selectedGame);
+  const selectedGame = useAppSelector((state) => state.board.selectedGame);
   const { data: session } = useSession();
 
   return (
-    <nav id="nav" className="nav w-[90%] my-0 mx-auto max-w-[1200px] py-5 px-0 grid grid-cols-[max-content_max-content] justify-between">
-      <Link href="/" className="mr-auto font-brand text-3xl text-stone-600 no-underline z-40">♞BOARDS <span className="inline font-texts ml-2 uppercase tracking-[2px] text-stone-600 text-[1rem] font-bold">{selectedGame}</span></Link>
-      <ul className="nav__links absolute bg-stone-200/90 inset-0 z-30 min-h-[500px] sm:min-h-[400px] py-0 pl-[5%] pr-0 grid gap-7 md:static md:min-h-0 md:p-0 md:bg-transparent md:grid-flow-col md:gap-[1em]">
-        <NavigationItem href='/' color="text-stone-600 hover:text-stone-800" text="home"/>
-        <NavigationItem href='/games' color="text-stone-600 hover:text-stone-800" text="games"/>
-        <NavigationItem href='/about' color="text-stone-600 hover:text-stone-800" text="about"/>
-        {!session ? 
-          <NavigationItem href='/login' color="text-sky-700 hover:text-sky-500" text="login"/>
-          :
-          <NavigationItem href='/account' color="text-sky-700 hover:text-sky-500" text={session.user.username}/>
-        }
+    <nav
+      id='nav'
+      className='nav w-[90%] my-0 mx-auto max-w-[1200px] py-5 px-0 grid grid-cols-[max-content_max-content] justify-between'
+    >
+      <Link
+        href='/'
+        className='mr-auto font-brand text-3xl text-stone-600 no-underline z-40'
+      >
+        ♞BOARDS{' '}
+        <span className='inline font-texts ml-2 uppercase tracking-[2px] text-stone-600 text-[1rem] font-bold'>
+          {selectedGame}
+        </span>
+      </Link>
+      <ul className='nav__links absolute bg-stone-200/90 inset-0 z-30 min-h-[500px] sm:min-h-[400px] py-0 pl-[5%] pr-0 grid gap-7 md:static md:min-h-0 md:p-0 md:bg-transparent md:grid-flow-col md:gap-[1em]'>
+        <NavigationItem
+          href='/'
+          color='text-stone-600 hover:text-stone-800'
+          text='home'
+        />
+        <NavigationItem
+          href='/games'
+          color='text-stone-600 hover:text-stone-800'
+          text='games'
+        />
+        <NavigationItem
+          href='/about'
+          color='text-stone-600 hover:text-stone-800'
+          text='about'
+        />
+        {!session ? (
+          <NavigationItem
+            href='/login'
+            color='text-sky-700 hover:text-sky-500'
+            text='login'
+          />
+        ) : (
+          <NavigationItem
+            href='/account'
+            color='text-sky-700 hover:text-sky-500'
+            text={'account'}
+          />
+        )}
       </ul>
-      <a href="#" className="nav__close place-self-center cursor-pointer z-40">
-        <Image src={CloseIcon} alt="Close icon" width={20} height={20} className="block w-5 h-5" /> 
+      <a href='#' className='nav__close place-self-center cursor-pointer z-40'>
+        <Image
+          src={CloseIcon}
+          alt='Close icon'
+          width={20}
+          height={20}
+          className='block w-5 h-5'
+        />
       </a>
-      <a href="#nav" className="nav__hamburguer place-self-center cursor-pointer z-40">
-        <Image src={HamburguerIcon} alt="Menu icon" width={20} height={20} className="block w-5 h-5" /> 
+      <a
+        href='#nav'
+        className='nav__hamburguer place-self-center cursor-pointer z-40'
+      >
+        <Image
+          src={HamburguerIcon}
+          alt='Menu icon'
+          width={20}
+          height={20}
+          className='block w-5 h-5'
+        />
       </a>
     </nav>
   );
