577d9c96fc84c58be17e82fcfc278e3c1e271ec2
Carlos <delgadociteriowa@gmail.com>
2026-05-10 19:06:40 -0300

    feat: sonner applied to delete board

--- src/app/hooks/useSavedBoards.ts
+++ src/app/hooks/useSavedBoards.ts
@@ -1,36 +1,49 @@
-import { useEffect } from "react";
-import { useSession } from "next-auth/react";
-import { useAppDispatch, useAppSelector } from "@/state/hooks";
-import { fetchSavedBoards } from "@/state/savedBoards/savedBoardsSlice";
-import { deleteBoard } from "@/state/board/boardSlice";
+import { deleteBoard } from '@/state/board/boardSlice';
+import { useAppDispatch, useAppSelector } from '@/state/hooks';
+import { fetchSavedBoards } from '@/state/savedBoards/savedBoardsSlice';
+import { useSession } from 'next-auth/react';
+import { useEffect } from 'react';
+import { toast } from 'sonner';
 
 export const useSavedBoards = () => {
   const { status } = useSession();
   const dispatch = useAppDispatch();
 
-  const { boards, loading, error } = useAppSelector(state => state.savedBoards);
+  const { boards, loading, error } = useAppSelector(
+    (state) => state.savedBoards,
+  );
 
   useEffect(() => {
-    if (status === "loading") return;
+    if (status === 'loading') return;
     dispatch(fetchSavedBoards());
   }, [status, dispatch]);
 
-  const handleDelete = async (id: string) => {
-    const confirmed = window.confirm(
-    "Are you sure you want to delete this board?"
-    );
-
-    if (!confirmed) return;
-    
-    await dispatch(deleteBoard(id));
-    dispatch(fetchSavedBoards());
+  const handleDelete = (id: string) => {
+    const toastId = toast('Delete saved board', {
+      description: 'Are you sure you want to delete this board?',
+      duration: Infinity,
+      action: {
+        label: 'delete',
+        onClick: async () => {
+          toast.dismiss(toastId);
+          await dispatch(deleteBoard(id));
+          dispatch(fetchSavedBoards());
+        },
+      },
+      cancel: {
+        label: 'cancel',
+        onClick: () => {
+          toast.dismiss(toastId);
+        },
+      },
+    });
   };
-  
+
   return {
     boards,
     loading,
     error,
     status,
-    handleDelete
-  }
-}; 
+    handleDelete,
+  };
+};
--- src/app/saved/page.tsx
+++ src/app/saved/page.tsx
@@ -1,32 +1,46 @@
-"use client";
+'use client';
 
-import Header from "@/components/Header";
-import Footer from "@/components/Footer";
-import LoadingComponent from "@/components/LoadingComponent";
-import SavedList from "@/components/SavedList";
-import { useSavedBoards } from "../hooks/useSavedBoards";
-import SectionTitle from "@/components/SectionTitle";
+import Footer from '@/components/Footer';
+import Header from '@/components/Header';
+import LoadingComponent from '@/components/LoadingComponent';
+import SavedList from '@/components/SavedList';
+import SectionTitle from '@/components/SectionTitle';
+import { Toaster } from 'sonner';
+import { useSavedBoards } from '../hooks/useSavedBoards';
 
 const Saved = () => {
   const { boards, loading, error, status, handleDelete } = useSavedBoards();
   return (
     <>
       <Header />
-      {(status === "loading") ?
+      {status === 'loading' ? (
         <LoadingComponent />
-        :
-        <main className="min-h-[800px]">
-          <section className="w-[90%] mx-auto max-w-[1200px] py-14 text-stone-600">
-            <SectionTitle title="saved games"/>
+      ) : (
+        <main className='min-h-[800px]'>
+          <section className='w-[90%] mx-auto max-w-[1200px] py-14 text-stone-600'>
+            <SectionTitle title='saved games' />
             <SavedList
               loading={loading}
               error={error}
               boards={boards}
               status={status}
-              handleDelete={handleDelete} />
+              handleDelete={handleDelete}
+            />
           </section>
+          <Toaster
+            position='top-center'
+            toastOptions={{
+              classNames: {
+                actionButton:
+                  '!bg-green-600 !text-white hover:!bg-green-500 !rounded-full order-1',
+                cancelButton:
+                  '!bg-sky-600 !text-white hover:!bg-sky-500 !rounded-full order-2 !ml-1',
+              },
+            }}
+            richColors
+          />
         </main>
-      }
+      )}
       <Footer />
     </>
   );
